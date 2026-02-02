df7a158786125ca00badab51c2edb884f1657641
ryukh1129 <ryukh1129@gmail.com>
2026-02-02 10:47:05 +0900

    feat: 회원가입 및 로그인 페이지 라우터 연결

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/home/HomePage";
 import BoardListPage from "../pages/boards/BoardListPage";
+import LoginPage from "../pages/auth/LoginPage";
+import SignUpPage from "../pages/auth/SignUpPage";
 
 function AppRoutes() {
     return (
         <Routes>
+            {/* Home */}
             <Route path="/" element={<HomePage />} />
+            {/* Auth */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            {/* Boards */}
             <Route path="/boards" element={<BoardListPage />} />
         </Routes>
     )
